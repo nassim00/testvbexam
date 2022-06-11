2f544ba010aa1a4724456fb6eab079fac75c4834
Nassim Filali <nassim.filali@student.ehb.be>
2022-06-11 18:52:24 +0200

    Initial commit

---
+++ src/main/java/be/ehb/testvbexam/TestvbexamApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.testvbexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestvbexamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestvbexamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ehb/testvbexam/TestvbexamApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.testvbexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestvbexamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
